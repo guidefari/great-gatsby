ebb6b83c346d9d5667eb055f6af1fd9b37ca2910
GT Fari <guideg6@gmail.com>
2020-07-09 17:47:54 +0200

    broke it again, nice

--- src/components/featuredItems/portfolioCard.js
+++ src/components/featuredItems/portfolioCard.js
@@ -2,19 +2,42 @@
 import React from 'react'
 // import {  graphql } from 'gatsby';
 import PropTypes from 'prop-types';
-import Layout from "../../components/layout";
 
-export default function portfolioCard({ data }) {
-    console.log(data)
-    return (
-      <Layout>
-        <div>Hello world</div>
-      </Layout>
+const  portfolioCard = ({ data }) =>
+    (
+      <div>
+      {data.allMarkdownRemark.edges.map(item => (
+        <div key={item.node.id} className="h-full w-full md:min-w-2xl p-4 flex flex-col-reverse justify-between md:flex-row bg-gray-600 md:max-w-3xl bg-white rounded-lg md:p-12 shadow-lg">
+        <div className="portfolioText flex flex-col justify-between text-center md:text-left md:w-8/12 mx-auto px-5">
+          <h1 className="text-2xl md:text-3xl font-bold mb-2">{{ item.node.title }}</h1>
+          <p className="mt-4 h-40 text-gray-400">  {{ item.Description }}  <br/><br/>
+            <span > 
+              Read more about this project <a href={item.External} className="text-yellow-600">here</a>
+            </span>
+            {/* <span v-else>
+              Check out the website<a :href="card.Link" className="text-yellow-600">here</a>
+            </span> */}
+          </p>
+          <div className="mt-8 w-full flex flex-row flex-wrap md:justify-start justify-center mx-auto">
+            <span v-for="tag in card.Tags" :key="tag" className="bg-gray-900 text-gray-100 px-5 mb-3 mr-4 py-2 font-semibold rounded">
+              {{ tag }}
+              </span>
+          </div>
+        </div>
+        <div className="md:w-4/12 m-auto w-48 h-56 md:h-auto flex flex-col justify-center align-middle">
+          <a href="#">
+            <img className="m-auto max-h-xs md:w-auto"  />
+          </a>
+        </div>
+      </div>
+      ))}
+      </div>
     )
-  }
-
-  portfolioCard.propTypes = {
-    data: PropTypes.array,
-  };
-
   
+
+portfolioCard.propTypes = {
+  data: PropTypes.array,
+};
+
+export default portfolioCard
+
