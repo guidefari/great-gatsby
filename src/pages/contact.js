3a70667b7a462dccad65d817e50903d8cb83e8f3
GT Fari <guideg6@gmail.com>
2020-07-15 09:57:10 +0200

    Blogcard first style iteration

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,28 +10,28 @@ function ContactPage() {
         keywords={[`gatsby`, `tailwind`, `react`, `tailwindcss`, `contact`]}
         title="Contact"
       />
-      <section className="section80 relative">
+      <section className="relative section80">
     
     <div className="container px-5 py-24 mx-auto">
-      <div className="flex flex-col text-center w-full mb-12">
-        <h1 className="md:text-5xl text-2xl uppercase font-black underline text-black">Contact Us</h1>
-        <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-black">Let&#39;s work together, leave a message below.</p>
+      <div className="flex flex-col w-full mb-12 text-center ">
+        <h1 className="text-2xl underline uppercase md:text-5xl">Contact Us</h1>
+        <p className="mx-auto text-base leading-relaxed text-black lg:w-2/3">Let&#39;s work together, leave a message below.</p>
       </div>
-      <div className="lg:w-1/2 md:w-2/3 mx-auto">
+      <div className="mx-auto lg:w-1/2 md:w-2/3">
         <div className="flex flex-wrap -m-2">
-          <div className="p-2 w-1/2">
-            <input className="w-full bg-gray-300 rounded border border-gray-700 text-gray-800 focus:outline-none focus:border-yellow-200 text-base px-4 py-2" placeholder="Name" type="text" ></input>
+          <div className="w-1/2 p-2">
+            <input className="w-full px-4 py-2 text-base text-gray-800 bg-gray-300 border border-gray-700 rounded focus:outline-none focus:border-yellow-200" placeholder="Name" type="text" ></input>
           </div>
-          <div className="p-2 w-1/2">
-            <input className="w-full bg-gray-300 rounded border border-gray-700 text-gray-800 focus:outline-none focus:border-yellow-200 text-base px-4 py-2" placeholder="Email" type="email" ></input>
+          <div className="w-1/2 p-2">
+            <input className="w-full px-4 py-2 text-base text-gray-800 bg-gray-300 border border-gray-700 rounded focus:outline-none focus:border-yellow-200" placeholder="Email" type="email" ></input>
           </div>
-          <div className="p-2 w-full">
-            <textarea className="w-full bg-gray-300 rounded border border-gray-700 text-gray-800 focus:outline-none h-48 focus:border-yellow-200 text-base px-4 py-2 resize-none block" placeholder="Message"></textarea>
+          <div className="w-full p-2">
+            <textarea className="block w-full h-48 px-4 py-2 text-base text-gray-800 bg-gray-300 border border-gray-700 rounded resize-none focus:outline-none focus:border-yellow-200" placeholder="Message"></textarea>
           </div>
-          <div className="p-2 w-full">
-            <button className="bg-orange-100 text-gray-600 font-semibold  flex mx-auto  border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">Submit</button>
+          <div className="w-full p-2">
+            <button className="flex px-8 py-2 mx-auto text-lg font-semibold text-gray-600 bg-orange-100 border-0 rounded focus:outline-none hover:bg-gray-400">Submit</button>
           </div>
-          <div className="p-2 w-full pt-8 mt-8 border-t border-gray-800 text-center">
+          <div className="w-full p-2 pt-8 mt-8 text-center border-t border-gray-800">
             
             
           </div>
