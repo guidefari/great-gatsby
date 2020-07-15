16bb3025b9c88834f37b6f38aa5c582a95c472a5
GT Fari <guideg6@gmail.com>
2020-07-15 10:51:11 +0200

    Blog card semi-done; no image yet

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -12,9 +12,9 @@ const BlogCard = ({category, title, slug, path}) => {
             <div className="h-full overflow-hidden border-2 border-gray-700 rounded-lg shadow-lg">
           <img className="object-cover object-center w-full lg:h-48 md:h-36" src="https://dummyimage.com/720x400" alt="blog" />
           <div className="p-6 bg-gray-600">
-          <h2 className="mb-1 text-xs font-medium tracking-widest text-gray-500 title-font">{category}</h2>
-            <h1 className="mb-3 text-lg font-medium text-gray-900 title-font">{title}</h1>
-            <p className="mb-3 leading-relaxed">{slug}</p>
+          <h2 className="mb-1 text-xs font-medium tracking-widest title-font">{category}</h2>
+            <h1 className="mb-3 text-xl font-medium text-gray-900 title-font">{title}</h1>
+            <p className="mb-3 leading-relaxed text-gray-900">{slug}</p>
             <div className="flex flex-wrap items-center ">
               <Link to={path} className="inline-flex items-center text-yellow-600 md:mb-2 lg:mb-0">Read full
                 <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
