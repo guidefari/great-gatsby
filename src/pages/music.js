ed6a54cea6b73b5aa0f3cff260913dcafb6ac30c
GT Fari <guideg6@gmail.com>
2020-07-15 11:33:06 +0200

    image source done

--- src/pages/music.js
+++ src/pages/music.js
@@ -51,6 +51,7 @@ query MusicIndexQuery {
           title
           date
           author
+          slug
           image
         }
       }
@@ -61,17 +62,3 @@ query MusicIndexQuery {
 
 
 export default MusicPage
-
-{/* <div key={post.node.id}>
-  <h3>{post.node.frontmatter.title}</h3>
-  <small>
-    Posted by {post.node.frontmatter.author} on{' '}
-    {post.node.frontmatter.date}
-  </small>
-  <br />
-  <br />
-  <Link to={post.node.frontmatter.path}>Read More</Link> */}
-  {/* <br />
-  <br />
-  <hr />
-</div> */}
