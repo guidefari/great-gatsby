4c9fe77043094c9a66e1538153b3fe53d993e883
GT Fari <guideg6@gmail.com>
2020-07-15 23:02:26 +0200

    Last commit of the day I swear

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -8,13 +8,15 @@ const BlogCard = ({category, title, slug, path, image}) => {
     // const post = data.allMarkdownRemark.edges.map()
     
     return (
-        <div>
-            <div className="h-full overflow-hidden border-2 border-gray-700 rounded-lg shadow-lg">
+        <div className='h-full'>
+            <div className="h-full overflow-hidden border-2 border-gray-700 rounded-lg shadow-lg ">
           <img className="object-cover object-center w-full lg:h-48 md:h-36" src={image} alt="blog" />
-          <div className="p-6 bg-gray-600">
-          <h2 className="mb-1 text-xs font-medium tracking-widest title-font">{category}</h2>
-            <h1 className="mb-3 text-xl font-medium text-gray-900 title-font">{title}</h1>
-            <p className="mb-3 leading-relaxed text-gray-900">{slug}</p>
+          <div className="flex flex-col justify-between p-6 bg-gray-600 min-h-xs">
+            <div>
+              <h2 className="mb-1 text-xs font-medium tracking-widest title-font">{category}</h2>
+              <h1 className="mb-3 text-xl font-medium text-gray-900 title-font">{title}</h1>
+              <p className="mb-3 leading-relaxed text-gray-900">{slug}</p>
+            </div>
             <div className="flex flex-wrap items-center ">
               <Link to={path} className="inline-flex items-center text-yellow-600 md:mb-2 lg:mb-0">Read full
                 <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
