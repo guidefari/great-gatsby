37ac41306b5ce75d956bfd28f290a8e55c5706ba
GT Fari <guideg6@gmail.com>
2020-07-20 08:30:28 +0200

    Contact form update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -19,16 +19,16 @@ function ContactPage() {
         <p className="mx-auto text-base leading-relaxed text-black lg:w-2/3">Let&#39;s work together, leave a message below.</p>
       </div>
 
-      <form className="mx-auto lg:w-1/2 md:w-2/3" name="contact" netlify>
+      <form className="mx-auto lg:w-1/2 md:w-2/3" name="contact" data-netlify="true" method='POST'>
         <div className="flex flex-wrap -m-2">
           <div className="w-1/2 p-2">
-            <input className="w-full px-4 py-2 text-base text-gray-800 bg-gray-300 border border-gray-700 rounded focus:outline-none focus:border-yellow-200" placeholder="Name" type="text" ></input>
+            <input className="w-full px-4 py-2 text-base text-gray-800 bg-gray-300 border border-gray-700 rounded focus:outline-none focus:border-yellow-200" placeholder="Name" type="text" name='name' ></input>
           </div>
           <div className="w-1/2 p-2">
-            <input className="w-full px-4 py-2 text-base text-gray-800 bg-gray-300 border border-gray-700 rounded focus:outline-none focus:border-yellow-200" placeholder="Email" type="email" ></input>
+            <input className="w-full px-4 py-2 text-base text-gray-800 bg-gray-300 border border-gray-700 rounded focus:outline-none focus:border-yellow-200" placeholder="Email" type="email" name='email' ></input>
           </div>
           <div className="w-full p-2">
-            <textarea className="block w-full h-48 px-4 py-2 text-base text-gray-800 bg-gray-300 border border-gray-700 rounded resize-none focus:outline-none focus:border-yellow-200" placeholder="Message"></textarea>
+            <textarea className="block w-full h-48 px-4 py-2 text-base text-gray-800 bg-gray-300 border border-gray-700 rounded resize-none focus:outline-none focus:border-yellow-200" placeholder="Message" name='message'></textarea>
           </div>
           <div className="w-full p-2">
             <button className="flex px-8 py-2 mx-auto text-lg font-semibold text-gray-600 bg-orange-100 border-0 rounded focus:outline-none hover:bg-gray-400" type='submit'>Submit</button>
