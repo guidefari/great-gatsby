00718b6509f195f3ebc5c5e9e77ba9feb9100732
GT Fari <guideg6@gmail.com>
2020-07-20 08:19:06 +0200

    contact form update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,11 +13,13 @@ function ContactPage() {
       <section className="relative section80">
     
     <div className="container px-5 py-24 mx-auto">
+
       <div className="flex flex-col w-full mb-12 text-center ">
         <h1 className="text-2xl underline uppercase md:text-5xl">Contact Us</h1>
         <p className="mx-auto text-base leading-relaxed text-black lg:w-2/3">Let&#39;s work together, leave a message below.</p>
       </div>
-      <div className="mx-auto lg:w-1/2 md:w-2/3">
+
+      <form className="mx-auto lg:w-1/2 md:w-2/3" name="contact" netlify>
         <div className="flex flex-wrap -m-2">
           <div className="w-1/2 p-2">
             <input className="w-full px-4 py-2 text-base text-gray-800 bg-gray-300 border border-gray-700 rounded focus:outline-none focus:border-yellow-200" placeholder="Name" type="text" ></input>
@@ -29,14 +31,13 @@ function ContactPage() {
             <textarea className="block w-full h-48 px-4 py-2 text-base text-gray-800 bg-gray-300 border border-gray-700 rounded resize-none focus:outline-none focus:border-yellow-200" placeholder="Message"></textarea>
           </div>
           <div className="w-full p-2">
-            <button className="flex px-8 py-2 mx-auto text-lg font-semibold text-gray-600 bg-orange-100 border-0 rounded focus:outline-none hover:bg-gray-400">Submit</button>
+            <button className="flex px-8 py-2 mx-auto text-lg font-semibold text-gray-600 bg-orange-100 border-0 rounded focus:outline-none hover:bg-gray-400" type='submit'>Submit</button>
           </div>
           <div className="w-full p-2 pt-8 mt-8 text-center border-t border-gray-800">
-            
-            
           </div>
         </div>
-      </div>
+      </form>
+
     </div>
 
   </section>
