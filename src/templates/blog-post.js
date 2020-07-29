948d3e197ec358ff9507566efe6ae9a97e6c2925
GT Fari <guideg6@gmail.com>
2020-07-29 14:46:44 +0200

    typo, lol

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -19,7 +19,7 @@ export default function Template({data}) {
             article />
             
           <div className='prose text-gray-400 lg:prose-xl'> 
-              <Img className='mb-10' fluid={post.frontmatter.image} alt={post.frontmatter.title} />
+              <Img className='mb-10' image={post.frontmatter.image} alt={post.frontmatter.title} />
               <h1 className=''>{post.frontmatter.title}</h1>
               <h5>By {post.frontmatter.author}</h5>
               <h5>On {post.frontmatter.date}</h5>
