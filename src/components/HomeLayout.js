833a2a463a8cbbf157c46a5e6aa405e250dbd8d2
GT Fari <guideg6@gmail.com>
2020-08-04 10:53:52 +0200

    footer update

--- src/components/HomeLayout.js
+++ src/components/HomeLayout.js
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 import React from "react";
+import gatsbylogo from '../../static/gatsbyLogo.svg';
 
 import Header from "./header";
 
 
 function HomeLayout({ children }) {
   return (
-    <div className="flex flex-col font-circular-black text-gray-900 bg-gray-700 min-h-screen">
+    <div className="flex flex-col min-h-screen text-gray-900 bg-gray-700 font-circular-black">
       <Header />
 
       <main className="flex-1 w-full max-w-full py-8 mx-auto md:py-16">
@@ -15,12 +16,14 @@ function HomeLayout({ children }) {
 
   <footer className="text-gray-700 body-font">
     <div className="bg-gray-200">
-      <div className=" container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
-        <p className="bg-yellow-200 text-gray-700 text-sm text-center sm:text-left">© 2020 Goosebumps Collective—
-          <a href="https://linktr.ee/goosebumps" className="text-gray-900 ml-1 hover:bg-yellow-300" target="_blank"
+      <div className="container flex flex-col-reverse px-5 py-4 mx-auto justify-evenly sm:flex-row">
+        <p className="text-sm text-center text-gray-700 sm:text-left">© 2020 Goosebumps Collective—
+          <a href="https://linktr.ee/goosebumps" className="ml-1 text-black hover:bg-yellow-300 " target="_blank"
             rel="noopener noreferrer">Social Links</a>
         </p>
+        <p className='text-sm text-center text-gray-700 sm:text-left'> Powered by:<a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer"><img className='inline-block w-16 h-4 text-sm text-center text-gray-700 transition duration-300 ease-out transform hover:scale-105 sm:text-left sm:w-20' src={gatsbylogo} alt="" /></a></p>
       </div>
+      
     </div>
   </footer>
 
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import React from "react";
+import gatsbylogo from '../../static/gatsbyLogo.svg';
 
 import Header from "./header";
 
@@ -12,14 +13,16 @@ function Layout({ children }) {
         {children}
       </main>
 
-  <footer className="text-gray-700 body-font">
+      <footer className="text-gray-700 body-font">
     <div className="bg-gray-200">
-      <div className="container flex flex-col flex-wrap px-5 py-4 mx-auto sm:flex-row">
-        <p className="text-sm text-center text-gray-700 bg-yellow-200 sm:text-left">© 2020 Goosebumps Collective—
-          <a href="https://linktr.ee/goosebumps" className="ml-1 text-gray-900 hover:bg-yellow-300" target="_blank"
+      <div className="container flex flex-col-reverse px-5 py-4 mx-auto justify-evenly sm:flex-row">
+        <p className="text-sm text-center text-gray-700 sm:text-left">© 2020 Goosebumps Collective—
+          <a href="https://linktr.ee/goosebumps" className="ml-1 text-black hover:bg-yellow-300 " target="_blank"
             rel="noopener noreferrer">Social Links</a>
         </p>
+        <p className='text-sm text-center text-gray-700 sm:text-left'> Powered by:<a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer"><img className='inline-block w-16 h-4 text-sm text-center text-gray-700 transition duration-300 ease-out transform hover:scale-105 sm:text-left sm:w-20' src={gatsbylogo} alt="" /></a></p>
       </div>
+      
     </div>
   </footer>
 
