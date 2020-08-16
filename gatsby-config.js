12c94e5c3b76065163decc32e7d0aa449b96c4e7
GT Fari <guideg6@gmail.com>
2020-08-16 23:49:41 +0200

    added responsive iframe plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,13 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [`gatsby-remark-responsive-iframe`],
+      },
+    },
+    //posts
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/posts`,
