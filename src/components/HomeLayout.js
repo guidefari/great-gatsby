0e2f6a238af4d44bf396c81258e64cd3d4db259e
GT Fari <guideg6@gmail.com>
2020-09-22 23:25:08 +0200

    extended thoughts article

--- src/components/HomeLayout.js
+++ src/components/HomeLayout.js
@@ -1,6 +1,5 @@
 import PropTypes from "prop-types";
 import React from "react";
-import gatsbylogo from '../../static/gatsbyLogo.svg';
 
 import Header from "./header";
 
@@ -17,11 +16,8 @@ function HomeLayout({ children }) {
   <footer className="text-gray-700 body-font">
     <div className="bg-gray-200">
       <div className="container flex flex-col-reverse px-5 py-4 mx-auto justify-evenly sm:flex-row">
-        <p className="text-sm text-center text-gray-700 sm:text-left">© 2020 Goosebumps Collective—
           <a href="https://linktr.ee/goosebumps" className="ml-1 text-black hover:bg-yellow-300 " target="_blank"
             rel="noopener noreferrer">Social Links</a>
-        </p>
-        <p className='text-sm text-center text-gray-700 sm:text-left'> Powered by:<a href="https://www.gatsbyjs.org/" target="_blank" rel="noopener noreferrer"><img className='inline-block w-16 h-4 text-sm text-center text-gray-700 link-transition hover:scale-105 sm:text-left sm:w-20' src={gatsbylogo} alt="" /></a></p>
       </div>
       
     </div>
