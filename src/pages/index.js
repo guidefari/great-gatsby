3656b8fe30d98af1ab38ee46477ef8952c3c3435
GT Fari <guideg6@gmail.com>
2020-10-28 12:08:58 +0200

    removed hero image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { graphql, Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import HomeLayout from "../components/HomeLayout";
 import SEO from "../components/seo";
-import Hero from "../images/space2.svg";
 import Feature from "../components/featureContainer";
 import PortfolioCard from "../components/portfolioCard";
 import PlaylistCard from "../components/playlistCard";
@@ -27,17 +26,11 @@ function IndexPage({data}) {
 
       <section className="text-center">
         <div className="inline-block max-h-screen">
-          <img
-            alt="Man with headphones in space"
-            className="block w-full mx-auto mb-8 md:max-w-eighty"
-            src={Hero}
-          />
-    
-          <h2 className="inline-block p-3 mb-4 text-2xl font-bold rounded-md bg-custom-yellow-500 text-custom-gray-bg">
+          <h1 className="inline-block p-3 mb-4 text-6xl font-bold text-white rounded-md">
             Welcome to GooseBumps Collective
-          </h2>
+          </h1>
           <br/>
-          <h3 className="inline-block p-3 mb-4 text-xl font-bold rounded-md bg-custom-yellow-500 text-custom-gray-bg"> Web Development & Music </h3>
+          <h3 className="inline-block p-3 mb-4 text-xl font-semibold text-white rounded-md"> Web Development, Music & everything between</h3>
         </div>
         <Feature bigHead="Featured Web Projects" smallHead="Portfolio">
             {data.portfolio.edges.map(port => (
@@ -63,9 +56,9 @@ function IndexPage({data}) {
       <section id="Contact" className="flex flex-row justify-center mb-24 border-gray-200 featured body-font">
       <div classNames="inline-block mx-auto text-center w-8 p-6 rounded-lg">
         <h1 className="mb-2 text-2xl font-bold text-center md:text-3xl">
-          Let&#39;s <span className="text-yellow-600">Talk</span>
+          Hire <span className="text-yellow-600">Me</span>
         </h1>
-        <p className="pb-6 text-center ">Got a project or collaboration you want to get started on?<br/> Send an <a href="mailto:admin@goosebumps.co.zw"><span className="text-yellow-500 hover:text-yellow-600">e-mail</span></a> and I&#39;ll get back to you as soon as possible.</p>
+        <p className="pb-6 text-center ">I am available for freelance/contract work, & full time work.<br/> Send an <a href="mailto:admin@goosebumps.co.zw"><span className="text-yellow-500 hover:text-yellow-600">e-mail</span></a> and I&#39;ll get back to you within a day or two.</p>
         <p className="pb-6 text-center ">If you prefer to use a contact form, <Link to='/contact' className="text-yellow-500 hover:text-yellow-600">here it is</Link>.</p>
       </div>
     </section>
