022e2aa0b3aa1a12decd551873f26ddfb596599a
Guide Fari <guideg6@gmail.com>
2021-06-01 04:31:30 +0200

    copy updates

--- src/components/about.js
+++ src/components/about.js
@@ -12,7 +12,7 @@ function About({image}) {
             <div className="flex flex-col justify-center max-w-md md:w-1/2">
                 <h2 className="text-2xl font-black text-left underline uppercase md:text-5xl font-circular-bold">About</h2>
                 <div className="mt-4 text-xl text-left">
-                    Built by: <a rel="noopener noreferrer" className=" link-transition about-link" href="https://twitter.com/txndai">GT Fari</a>. That guy on the right, or the bottom🤷‍♂️<br></br><br></br>
+                    Built by: <a rel="noopener noreferrer" className=" link-transition about-link" href="https://twitter.com/txndai">Guide Fari</a>. That guy on the right, or the bottom🤷‍♂️<br></br><br></br>
                     Tech used: 
                     <ul>
                         <li><a rel="noopener noreferrer" className="about-link link-transition" href="https://www.gatsbyjs.org/"> Gatsby</a></li>
@@ -21,8 +21,8 @@ function About({image}) {
                         <strike><li><a rel="noopener noreferrer" className="about-link link-transition" href="https://parceljs.org/">ParcelJS</a></li></strike>
                         <li><a rel="noopener noreferrer" className="about-link link-transition" href="https://www.netlify.com/">Netlify</a></li>
                     </ul> <br></br><br></br>
-                    <p>Check out the <Link className="about-link link-transition" to="/blog"> blog</Link> for extended thoughts on this & that.</p><br/>
-                    <p>Did I mention that this site is open source? You can find the repo <a rel="noopener noreferrer" className="about-link link-transition" href='https://github.com/txndai/great-gatsby'>here</a></p>
+                    <p>Check out the <Link className="about-link link-transition" to="/blog">&apos;not blog&apos;</Link> for extended thoughts on this & that.</p><br/>
+                    <p>This site is open source😎 You can find the repo <a rel="noopener noreferrer" className="about-link link-transition" href='https://github.com/txndai/great-gatsby'>here</a></p>
                 </div>
             </div>
             <div className="flex w-full -mt-5 md:justify-end md:w-1/2">
@@ -33,7 +33,7 @@ function About({image}) {
                             <blockquote>
                                 This site was born out of necessity, mainly as a site for me to practice what I learn on the
                                 Web Development journey, and to serve as a central house for music related content & other
-                                projects. - A digital playground, so to say.
+                                projects. - A digital playground.
                             </blockquote>
                         </div>
                     </div>
